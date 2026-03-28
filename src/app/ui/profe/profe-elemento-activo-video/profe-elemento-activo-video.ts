0b68973934621f255b9d98ec4d9c30aaf0d3c10b
folmoortiz <desarrollo@afosoft.com>
2026-03-27 21:22:32 -0500

    Youtube player

--- src/app/ui/profe/profe-elemento-activo-video/profe-elemento-activo-video.ts
+++ src/app/ui/profe/profe-elemento-activo-video/profe-elemento-activo-video.ts
@@ -1,4 +1,4 @@
-import { Component, input, OnInit } from '@angular/core';
+import { Component, effect, ElementRef, input, signal, viewChild } from '@angular/core';
 
 @Component({
   selector: 'app-profe-elemento-activo-video',
@@ -6,13 +6,45 @@ import { Component, input, OnInit } from '@angular/core';
   templateUrl: './profe-elemento-activo-video.html',
   styleUrl: './profe-elemento-activo-video.scss',
 })
-export class ProfeElementoActivoVideo implements OnInit {
+export class ProfeElementoActivoVideo {
 
   video = input.required<string>();
-  
-  ngOnInit() {
-    
+  // Obtenemos el contenedor usando Signals
+  playerContainer = viewChild<ElementRef>('playerContainer');
+
+  private player?: YT.Player;
+  isPlayerReady = signal(false);
+
+  constructor() {
+    // Un efecto que reacciona cuando el elemento del DOM está disponible
+    effect(() => {
+      const container = this.playerContainer();
+      if (container) {
+        this.initYoutubePlayer(container.nativeElement);
+      }
+    });
   }
 
+  initYoutubePlayer(element: HTMLElement) {
+    // La API de YT llama a esta función global cuando está lista
+    // En una app real, podrías necesitar verificar si ya existe YT.Player
+    this.player = new YT.Player(element, {
+      height: '100%',
+      width: '100%',
+      videoId: this.video(), // ID del video
+      events: {
+        'onReady': () => { this.isPlayerReady.set(true); this.playVideo(); },
+        'onStateChange': (event) => console.log('Estado:', event.data)
+      }
+    });
+  }
+
+  playVideo() {
+    this.player?.playVideo();
+  }
+
+  pauseVideo() {
+    this.player?.pauseVideo();
+  }
 
 }
