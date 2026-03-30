f68a42bac8c89c1adbb5a0767d63a982523e0031
folmoortiz <desarrollo@afosoft.com>
2026-03-30 18:40:43 -0500

    Fullscreen profeweb

--- src/app/ui/profe/profe-elemento-activo-video/profe-elemento-activo-video.ts
+++ src/app/ui/profe/profe-elemento-activo-video/profe-elemento-activo-video.ts
@@ -1,37 +1,52 @@
-import { Component, effect, ElementRef, input, signal, viewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, effect, ElementRef, input, OnDestroy, signal, viewChild } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-profe-elemento-activo-video',
-  imports: [],
+  imports: [CommonModule, MatButtonModule, MatIconModule],
   templateUrl: './profe-elemento-activo-video.html',
   styleUrl: './profe-elemento-activo-video.scss',
 })
-export class ProfeElementoActivoVideo {
+export class ProfeElementoActivoVideo implements OnDestroy {
 
   video = input.required<string>();
-  // Obtenemos el contenedor usando Signals
   playerContainer = viewChild<ElementRef>('playerContainer');
 
   private player?: YT.Player;
   isPlayerReady = signal(false);
+  fullscreen = signal(false);
 
   constructor() {
-    // Un efecto que reacciona cuando el elemento del DOM está disponible
     effect(() => {
       const container = this.playerContainer();
+      const videoId = this.video();
+
       if (container) {
-        this.initYoutubePlayer(container.nativeElement);
+        if (!this.player) {
+          // Primera vez: Creamos el reproductor
+          this.initYoutubePlayer(container.nativeElement, videoId);
+        } else {
+          // Siguientes veces: El reproductor ya existe, solo cambiamos el video
+          this.player.loadVideoById(videoId);
+        }
       }
     });
   }
 
-  initYoutubePlayer(element: HTMLElement) {
-    // La API de YT llama a esta función global cuando está lista
-    // En una app real, podrías necesitar verificar si ya existe YT.Player
+  ngOnDestroy() {
+    if (this.player) {
+      this.player.destroy();
+    }
+  }
+
+  initYoutubePlayer(element: HTMLElement, video: string) {
     this.player = new YT.Player(element, {
       height: '100%',
       width: '100%',
-      videoId: this.video(), // ID del video
+      playerVars: { 'autoplay': 1, 'controls': 0, 'rel': 0, 'showinfo': 0 },
+      videoId: video,
       events: {
         'onReady': () => { this.isPlayerReady.set(true); this.playVideo(); },
         'onStateChange': (event) => console.log('Estado:', event.data)
@@ -43,8 +58,8 @@ export class ProfeElementoActivoVideo {
     this.player?.playVideo();
   }
 
-  pauseVideo() {
+  /* pauseVideo() {
     this.player?.pauseVideo();
-  }
+  } */
 
 }
